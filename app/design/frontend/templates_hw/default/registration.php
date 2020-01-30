990f7a211af13ef23abf89f90f2d7ee2f3e8c771
worzewolf <ivan.havryliuk95@gmail.com>
2020-01-30 23:41:36 +0200

    rendered 3 templates according to hw tasks

---
+++ app/design/frontend/templates_hw/default/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ *template_hw theme.
+ *@author Ivan Havryliuk ivan.havryliuk95@gmail.com.
+ *@copyright 2020 worzewolf.
+*/
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/templates_hw/default', __DIR__);
