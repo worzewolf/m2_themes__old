f922c201d2e10f3a0aeeea4dcf6aa158d7c5f471
worzewolf <ivan.havryliuk95@gmail.com>
2020-03-20 19:03:22 +0200

    less cleance

--- app/design/frontend/avto/avtostore/Magento_Catalog/web/js/slider.js
+++ app/design/frontend/avto/avtostore/Magento_Catalog/web/js/slider.js
@@ -6,18 +6,14 @@ define([
     ],
     function ($) {
         'use strict';
-        var el = document.location.href;
-        console.log(el.substring(0, el.length - 1));
-        $('#slider-range').slider({
+        var element = $('#slider-range');
+        element.slider({
             min: 0,
             max: 700,
             values: [ 0, 200 ],
             slide: function( event, ui ) {
-                var activeFiltr = $('.filterHref');
                 $( "#amount" ).text( "$" + ui.values[ 0 ]);
                 $( "#amount2" ).text( "$" + ui.values[ 1 ]);
-                var hrefFilter = "http://magento2.local/women/tops-women.html?price="+ui.values[ 0 ]+'-'+ui.values[ 1 ];
-                activeFiltr.attr('href',hrefFilter);
             },
         });
     });
